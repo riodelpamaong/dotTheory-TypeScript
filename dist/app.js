6d8729964d16cb30aff058876de84559b13db3c5
Rio Del Pamaong <riodelpamaong@Rio-Del-Pamaong-MBP16.local>
2023-05-30 12:12:50 +0800

    Add next-generation functions
    - Implemented an arrow function that accepts a default parameter and a rest parameter.
    - Implemented an arrow function that returns an array and created variables to accept the destructured values of the function.
    - Implemented an arrow function that returns an object and created variables to accept the destructured values of the function.
    
    Ticket: JS-770

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,45 @@
 "use strict";
-console.log('Your code goes here...');
+// Function that accepts a default parameter and a rest parameters
+var greetUser = function (name) {
+    if (name === void 0) { name = "Anonymous"; }
+    var additionalNames = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        additionalNames[_i - 1] = arguments[_i];
+    }
+    console.log("Hello, ".concat(name, "!"));
+    if (additionalNames.length > 0) {
+        console.log("Additional names:", additionalNames.join(", "));
+    }
+};
+console.log("`Function that accepts a default parameter and a rest parameters`");
+// Example usage
+greetUser();
+greetUser("John");
+greetUser("Alice", "Bob", "Charlie");
+// Function that returns an array, and create variables that will accept the destructured values of the said function
+var getArrayData = function () {
+    // Generate array data
+    var stringValue = "Hello";
+    var numericValue = 42;
+    var booleanValue = true;
+    return [stringValue, numericValue, booleanValue];
+};
+var _a = getArrayData(), stringValue = _a[0], numericValue = _a[1], booleanValue = _a[2];
+console.log("`Function that returns an array, and create variables that will accept the destructured values of the said function`");
+console.log("string value: ", stringValue);
+console.log("numberic value: ", numericValue);
+console.log("boolean value: ", booleanValue);
+// Function that returns an object, and create variables that will accept the destructured values of the said function
+var getObjectData = function () {
+    // Generate object data
+    var name = "John Doe";
+    var age = 25;
+    var city = "New York";
+    return { name: name, age: age, city: city };
+};
+var _b = getObjectData(), userName = _b.name, age = _b.age, city = _b.city;
+console.log("`Function that returns an object, and create variables that will accept the destructured values of the said function`");
+console.log("name: ", userName);
+console.log("age: ", age);
+console.log("city:", city);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,61 @@
-console.log('Your code goes here...');
+// Function that accepts a default parameter and a rest parameters
+const greetUser = (
+  name: string = "Anonymous",
+  ...additionalNames: string[]
+): void => {
+  console.log(`Hello, ${name}!`);
+  if (additionalNames.length > 0) {
+    console.log("Additional names:", additionalNames.join(", "));
+  }
+};
+
+console.log(
+  "`Function that accepts a default parameter and a rest parameters`"
+);
+
+// Example usage
+greetUser();
+greetUser("John");
+greetUser("Alice", "Bob", "Charlie");
+
+// Function that returns an array, and create variables that will accept the destructured values of the said function
+const getArrayData = (): [string, number, boolean] => {
+  // Generate array data
+  const stringValue = "Hello";
+  const numericValue = 42;
+  const booleanValue = true;
+
+  return [stringValue, numericValue, booleanValue];
+};
+
+const [stringValue, numericValue, booleanValue] = getArrayData();
+
+console.log(
+  "`Function that returns an array, and create variables that will accept the destructured values of the said function`"
+);
+console.log("string value: ", stringValue);
+console.log("numberic value: ", numericValue);
+console.log("boolean value: ", booleanValue);
+
+// Function that returns an object, and create variables that will accept the destructured values of the said function
+const getObjectData = (): {
+  name: string;
+  age: number;
+  city: string;
+} => {
+  // Generate object data
+  const name = "John Doe";
+  const age = 25;
+  const city = "New York";
+
+  return { name, age, city };
+};
+
+const { name: userName, age, city } = getObjectData();
+
+console.log(
+  "`Function that returns an object, and create variables that will accept the destructured values of the said function`"
+);
+console.log("name: ", userName);
+console.log("age: ", age);
+console.log("city:", city);
